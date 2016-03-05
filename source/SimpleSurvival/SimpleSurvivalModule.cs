3848b0cac2976a055b61463b0cfd878fe7882a3a
Moonshot <moonshot11@users.noreply.github.com>
2016-03-05 14:50:15 -0800

    Kerbals die/respawn appropriately without LifeSupport

--- source/SimpleSurvival/SimpleSurvivalModule.cs
+++ source/SimpleSurvival/SimpleSurvivalModule.cs
@@ -10,26 +10,32 @@ namespace SimpleSurvival
     {
         public void FixedUpdate()
         {
+            // 10 unit(s) = 1 Kerbal for 1 day
             float RATE = 10f / 3600f / 6f;
             float ret_rs = part.RequestResource("LifeSupport", part.protoModuleCrew.Count * RATE * TimeWarp.fixedDeltaTime);
+
+            List<ProtoCrewMember> part_crew = part.protoModuleCrew;
 
             if (ret_rs == 0f)
             {
                 List<ProtoCrewMember> remove_kerbs = new List<ProtoCrewMember>();
 
-                foreach (ProtoCrewMember kerb in part.protoModuleCrew)
+                while (part_crew.Count > 0)
                 {
-                    // Move so list isn't being modified in place
-                    // Necessary - otherwise Kerbals are "K.I.A."
-                    // for future assignment
-                    remove_kerbs.Add(kerb);
-                    kerb.rosterStatus = ProtoCrewMember.RosterStatus.Dead;
-                    kerb.Die();
-                }
+                    ProtoCrewMember kerbal = part_crew[0];
+                    bool respawn_flag = HighLogic.CurrentGame.Parameters.Difficulty.MissingCrewsRespawn;
 
-                // Remove Kerbals from part
-                foreach (ProtoCrewMember kerb in remove_kerbs)
-                    part.RemoveCrewmember(kerb);
+                    // Kerbal must be removed from part BEFORE calling Die()
+                    part.RemoveCrewmember(kerbal);
+                    // ...for some reason
+                    kerbal.Die();
+
+                    // Put Kerbal in Missing queue
+                    if (respawn_flag)
+                        kerbal.StartRespawnPeriod();
+
+                    
+                }
             }
         }
     }
