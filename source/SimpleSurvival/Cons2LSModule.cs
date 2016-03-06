0a7389d8fd331f0d2ee4dfddc8453961480d3e78
Moonshot <moonshot11@users.noreply.github.com>
2016-03-05 17:42:54 -0800

    Created Cons2LSModule, added to HSC

---
+++ source/SimpleSurvival/Cons2LSModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSurvival
+{
+    public class Cons2LSModule : PartModule
+    {
+    }
+}
