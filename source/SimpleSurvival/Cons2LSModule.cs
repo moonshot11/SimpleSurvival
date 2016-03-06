f52c293ab09a2ea2a0cbbe0fa7374fd40264317a
Moonshot <moonshot11@users.noreply.github.com>
2016-03-05 18:51:29 -0800

    Added dummy code for Converter button

--- source/SimpleSurvival/Cons2LSModule.cs
+++ source/SimpleSurvival/Cons2LSModule.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace SimpleSurvival
 {
+    public enum ConverterStatus
+    {
+        READY,
+        CONVERTING,
+        NO_ELECTRICITY,
+        NO_CONSUMABLES
+    }
+
     public class Cons2LSModule : PartModule
     {
+        [KSPField(guiActive = true, guiName = "Status")]
+        string str_status = "";
+
+        ConverterStatus status = ConverterStatus.CONVERTING;
+
+        [KSPEvent(guiActive = true, guiActiveEditor = false, guiName = "Convert Consumables")]
+        public void ToggleStatus()
+        {
+            status = status == ConverterStatus.NO_CONSUMABLES ? ConverterStatus.NO_ELECTRICITY : ConverterStatus.NO_CONSUMABLES;
+        }
+
+        public void FixedUpdate()
+        {
+            //
+        }
+
+        /// <summary>
+        /// Generic part update. Handle part status.
+        /// </summary>
+        public override void OnUpdate()
+        {
+            str_status = StatusToString(status);
+            base.OnUpdate();
+        }
+
+        public string StatusToString(ConverterStatus st)
+        {
+            switch(st)
+            {
+                case ConverterStatus.CONVERTING:
+                    return "Converting";
+                case ConverterStatus.NO_CONSUMABLES:
+                    return "Insufficient Consumables";
+                case ConverterStatus.NO_ELECTRICITY:
+                    return "Insufficient Electricity";
+                case ConverterStatus.READY:
+                    return "Ready";
+                default:
+                    return "ERROR ConverterStatus";
+            }
+        }
     }
 }
