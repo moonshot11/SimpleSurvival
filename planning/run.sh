37a74e2c67298127e1cb544b9b9bf82de46843a6
Moonshot <moonshot11@users.noreply.github.com>
2018-02-19 18:15:53 -0800

    Update LS capacity, add price to CSV
    Update run.sh to output parts.cfg to CWD

--- planning/run.sh
+++ planning/run.sh
@@ -3,5 +3,5 @@
 # Run this to automatically overwrite the parts cfg
 # in the GameData path
 
-./gen_ls_cfg.pl -f ls_amts.csv > ../GameData/SimpleSurvival/Parts/parts.cfg
+./gen_ls_cfg.pl -f ls_amts.csv > parts.cfg
 echo Done!
