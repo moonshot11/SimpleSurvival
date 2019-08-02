783c58f655ed45acd294b895468e2d13e793ff05
Moonshot <moonshot11@users.noreply.github.com>
2019-08-01 21:55:21 -0700

    Change from .NET 4.0 to 3.5

--- source/SimpleSurvival/TextureUtil.cs
+++ source/SimpleSurvival/TextureUtil.cs
@@ -4,7 +4,6 @@ using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace SimpleSurvival
